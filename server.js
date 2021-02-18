f763cc1c7da46de57cef659dde1297f08390ad04
jmcia2020 <jeanmmcmahon@gmail.com>
2021-02-17 23:05:06 -0600

    fixed server

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+const express = require('express');
+
 const PORT = process.env.PORT || 3333;
+const app = express();
 
 // Start the Server
-app.listen(PORT, () => console.log('we are on the PORT ${PORT}'));
+app.listen(PORT, () => console.log(`we are on ${PORT}`));
 // This is the server being built. Now we need the routes.
 
 // Routes
 
-app.get('/' (request,response) => { response.send('Welcome to the Home Page!');
+app.get('/', (request,response) => {
+  response.send('Welcome to the Home Page!');
 });
 
 // Functions
-function Location(search_query), formatted_query, lat, lon){
+function Location(search_query, formatted_query, lat, lon) {
     this.search_query = search_query;
     this.formatted_query = formatted_query;
     this.lat = lat;
     this.lon = lon;
 }
 
-function Weather(search_query), formatted_query, city_name, lat, lon){
+function Weather(search_query, formatted_query, city_name, lat, lon) {
     this.search_query = search_query;
     this.formatted_query = formatted_query;
     this.city_name = city_name
